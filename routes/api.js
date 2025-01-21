4b05a940635661b66e5d697fbc5bb62a0f11c0bc
Simon Bouchard <simon.bouchard31@gmail.com>
2025-01-21 16:34:00 -0500

    add routing

--- routes/api.js
+++ routes/api.js
@@ -4,10 +4,33 @@ const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
   
-  const translator = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
+
+		let {text, locale} = req.body;
+		if (!text) {
+			return res.json({error: 'No text to translate'})
+		}
+
+		if (!text || !locale) {
+			return res.json({error: 'Required field(s) missing'})
+		}
+
+		if (locale !== 'american-to-british' && locale !== 'british-to-american') {
+			return res.json({error: 'Invalid value for locale field'})
+		}
+
+		let translator = new Translator(text, locale);
+
+		const translation = translator.translate(true);
+		/*
+		if (translation === text) {
+			return res.json({text: text, translation: 'Everything looks good to me!'})
+		}
+		*/
+
+		return res.json({text: text, translation: translation})
       
     });
 };
